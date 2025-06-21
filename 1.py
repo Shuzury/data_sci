26a15af752c79d1eba291071940cb6684128d7d3
Shuzury <shuzury57@gmail.com>
2025-06-21 19:03:19 +0530

    second commit

--- 1.py
+++ 1.py
@@ -1,3 +1,17 @@
+
 import numpy as np
-a=np.array([1,2,3,4,5,6,7,])
+# a=np.array([1,2,3,4,5,6,7,])
+# print(a)
+
+# b=np.array([[1,2,3],[4,5,6]])
+# print(b)
+
+a=np.full((2,7),7)
 print(a)
+
+b=np.eye(7)
+print(b)
+
+c=np.random.
+
+

00b354713d88eb5c66086d99b9acb32294dac48a
Shuzury <shuzury57@gmail.com>
2025-06-24 21:30:51 +0530

    numpy samapt

---
+++ numpy/1.py
@@ -0,0 +1,22 @@
+
+import numpy as np
+# # a=np.array([1,2,3,4,5,6,7,])
+# # print(a)
+
+# # b=np.array([[1,2,3],[4,5,6]])
+# # print(b)
+
+# # a=np.full((2,7),7)
+# # print(a)
+
+# # b=np.eye(7)
+# # print(b)
+
+# c=np.random.rand(7)
+# print(c)
+
+# d=np.random.rand(2,2)
+# print(d)
+
+a=np.arange(1,101)/100
+print(a)

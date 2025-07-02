92ae9063bac399143c45efd4e5fe2d0575929236
Shuzury <shuzury57@gmail.com>
2025-07-03 01:07:09 +0530

    finished social media project

--- kaggle/one/one.py
+++ kaggle/one/one.py
@@ -1,4 +1,3 @@
-from os import dup
 import numpy as np
 import pandas as pd 
 
@@ -32,5 +31,49 @@ df=pd.read_csv("kaggle/one/one.csv")
 # print(es)
 
 '''5. DESCRIPTIVE STATISTICS'''
+print(df["Age"].mean())
+print(df["Age"].mode()[0])
+print(df["Gender"].value_counts())
+print(df["Avg_Daily_Usage_Hours"].mean())
+print(df["Avg_Daily_Usage_Hours"].mode()[0])
+print(df["Mental_Health_Score"].mean())
 
+'''6. Platform Stats'''
+pf = df['Most_Used_Platform'].value_counts()
+print(pf)
+print(pf.idxmax(), pf.max())
+print(pd.crosstab(df['Most_Used_Platform'], df['Gender']))
+
+''' 7. Usage Patterns '''
+print(df.groupby('Academic_Level')["Avg_Daily_Usage_Hours"].mean())
+print(df.groupby('Gender')["Avg_Daily_Usage_Hours"].mean())
+df["Age_Group"] = pd.cut(df["Age"], bins=[17, 19, 21, 24, 100], labels=["18-19", "20-21", "22-24", "25+"])
+print(df.groupby("Age_Group")["Avg_Daily_Usage_Hours"].mean())
+
+'''8. Academic Impact'''
+print(df['Affects_Academic_Performance'].value_counts())
+print(df.groupby('Affects_Academic_Performance')['Avg_Daily_Usage_Hours'].mean())
+
+'''9. Sleep & Mental Health'''
+print(df["Avg_Daily_Usage_Hours"].corr(df["Sleep_Hours_Per_Night"]))
+print(df["Avg_Daily_Usage_Hours"].corr(df["Mental_Health_Score"]))
+df["Sleep_Group"] = pd.cut(df["Sleep_Hours_Per_Night"], bins=[0, 6, 8, 24], labels=["<6", "6-8", "8+"])
+print(df.groupby("Sleep_Group")["Mental_Health_Score"].mean())
+
+'''10. Country Analysis'''
+ct = df["Country"].value_counts()
+print(ct.head())
+top = ct.head().index
+print(df[df["Country"].isin(top)].groupby("Country")["Avg_Daily_Usage_Hours"].mean())
+
+'''11. Relationship Status'''
+rs = df["Relationship_Status"].value_counts()
+print(rs)
+print(df.groupby("Relationship_Status")["Avg_Daily_Usage_Hours"].mean())
+print(df.groupby("Relationship_Status")["Addicted_Score"].mean())
+
+'''12. New Categories'''
+df["Usage_Category"] = pd.cut(df["Avg_Daily_Usage_Hours"], bins=[0,3,6,float("inf")], labels=["Light","Moderate","Heavy"])
+print(df["Usage_Category"].value_counts())
+print(df["Age_Group"].value_counts())
 
